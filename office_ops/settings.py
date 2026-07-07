e02bdbf5618fd2ca93ff01692fb3084c56575b4e
Ajay Raman <your-github-email@gmail.com>
2026-07-07 16:57:18 +0530

    Configure settings for Render

--- office_ops/settings.py
+++ office_ops/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ.get("SECRET_KEY", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS
 ALLOWED_HOSTS = ['*']
